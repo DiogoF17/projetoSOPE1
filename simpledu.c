0d638d9e85492166ca49b492d0eed3428ef2fd18
Jessica <jessymireie@github.com>
2020-04-04 16:39:48 +0100

    sem aviso

--- simpledu.c
+++ simpledu.c
@@ -10,7 +10,7 @@
 #include <fcntl.h> 
 #include <math.h>
 #include <errno.h>
-#include <time.h>
+#include <sys/time.h>
 
 //----------------------------------------------------------------
 //      FUNÇÕES DE VALIDAÇÃO
