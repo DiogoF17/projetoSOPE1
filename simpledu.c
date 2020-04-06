20a36659f2d4ea2c7110accfd2f065ef57d571b8
Diogo Santos <diogofilipe1_santos@hotmail.com>
2020-04-06 20:32:51 +0100

    Quase fim

--- simpledu.c
+++ simpledu.c
@@ -433,8 +433,8 @@ void fazWait(){
         val = wait(&exit_status);
         if(val == -1 && errno == ECHILD)
             break;
-        else
-            exit_file(exit_status, val);
+        //else
+           // exit_file(exit_status, val);
     }
 
 }
@@ -461,7 +461,7 @@ void sigIntHandler(int signal){
     size_t len;
     int num;
 
-    recv_signal_file(SIGINT);
+    //recv_signal_file(SIGINT);
 
     if(getenv("PIDGROUP") == NULL){
         printf("Erro\n");
@@ -475,7 +475,7 @@ void sigIntHandler(int signal){
             perror("Kill");
             exit(3);
         }
-        send_signal_file(SIGSTOP, -num);
+        //send_signal_file(SIGSTOP, -num);
     }
 
     printf("\n\n######################################################\n\t\tMenu de Saida\n######################################################\n");
@@ -496,14 +496,14 @@ void sigIntHandler(int signal){
             perror("Kill");
             exit(3);
         }
-        send_signal_file(SIGCONT, -num);
+        //send_signal_file(SIGCONT, -num);
     }
     else if(num != 0){
          if(kill(-num, SIGTERM) == -1){
             perror("Kill");
             exit(3);
         }
-        send_signal_file(SIGTERM, -num);
+        //send_signal_file(SIGTERM, -num);
         exit(0);
     }
 
@@ -620,7 +620,7 @@ int main(int argc, char *argv[], char *envp[]){
     if(notOrig == 0){
         original = 1;
 
-        file_open_new_empty();
+        //file_open_new_empty();
 
         //Verifica se esta num formato valido
         validFormat(argc, argv, ind);
@@ -723,7 +723,7 @@ int main(int argc, char *argv[], char *envp[]){
                         arraPass[DIRSYMB] = printSymbolicDir(arraPass, dentry->d_name, d);
                 }
 
-                create_file(arraPass, getpid());
+                //create_file(arraPass, getpid());
 
                 execve(strcat(path,"/simpledu"), arraPass, envp);
                 perror("execvp");
@@ -816,7 +816,7 @@ int main(int argc, char *argv[], char *envp[]){
                                 arraPass[DIRSYMB] = printSymbolicDir(arraPass, dentry->d_name, d);
                         }
 
-                        create_file(arraPass, getpid());
+                        //create_file(arraPass, getpid());
 
                         execve(strcat(path,"/simpledu"), arraPass, envp);
                         perror("execvp");
@@ -906,7 +906,7 @@ int main(int argc, char *argv[], char *envp[]){
                     getline(buffer,&len,receiver);
                     num2 = atoi(buffer[0]);
                     somaBlocks += num2;
-                    send_pipe_file(num1, num2);
+                    //send_pipe_file(num1, num2);
                 }
                 close(fd[READ]);
             }
@@ -916,15 +916,15 @@ int main(int argc, char *argv[], char *envp[]){
             if(atoi(arraPass[m]) != -1){
                 if(atoi(arraPass[B]) >= 1){
                     printf("B%-10d%s\n",(int)ceil(somaBlocks * 1024 / atoi(arraPass[B])), printSymbolicDir(arraPass, dentry->d_name, d));
-                    entry_file(directory, (int)ceil(somaBlocks * 1024 / atoi(arraPass[B])));
+                    //entry_file(directory, (int)ceil(somaBlocks * 1024 / atoi(arraPass[B])));
                 }
                 else if(atoi(arraPass[b]) != 1){
                     printf("%-10d%s\n",somaBlocks, printSymbolicDir(arraPass, dentry->d_name, d));
-                    entry_file(directory, somaBlocks);
+                    //entry_file(directory, somaBlocks);
                 }
                 else if(atoi(arraPass[b]) == 1){
                     printf("b%-10d%s\n",somaSize, printSymbolicDir(arraPass, dentry->d_name, d));
-                    entry_file(directory, somaSize);
+                    //entry_file(directory, somaSize);
                 }
             }
             
@@ -938,7 +938,7 @@ int main(int argc, char *argv[], char *envp[]){
         size_t len;
         sprintf(msg,"%d\n%d\n",somaSize,somaBlocks);
         len = strlen(msg);
-        recv_pipe_file(somaSize, somaBlocks);
+        //recv_pipe_file(somaSize, somaBlocks);
         write(STDOUT_FILENO,msg,len);
     }
 
